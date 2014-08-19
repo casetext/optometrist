52a5be00cf990bbf1d1bf63d0e5047ef14b775fc
Harry Schmidt <goldibex@casetext.com>
2014-08-18 19:46:18 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,150 @@
+
+'use strict';
+
+function dejsonize(str) {
+
+  var rv;
+  try {
+    rv = JSON.parse(str);
+  } catch(e) {
+    try {
+      rv = JSON.parse('"' + str.replace(/"/g, '\"') + '"');
+    } catch(e) {
+      rv = null;
+    }
+  }
+
+  return rv;
+
+}
+
+function camelize(str) {
+
+  return str.replace(/(?:[_-])(\w)/g, function (_, c) {
+    return c ? c.toUpperCase () : '';
+  });
+
+}
+
+function argize(str) {
+
+  return str.replace(/([a-z])([A-Z])/g, function(_, l, u) {
+    return l + '-' + u.toLowerCase();
+  }).toLowerCase();
+
+}
+
+function envize(str) {
+
+  return str.replace(/([a-z])([A-Z])/g, function(_, l, u) {
+    return l.toUpperCase() + '_' + u;
+  }).toUpperCase();
+
+}
+
+
+module.exports = {
+
+  get: function(schema) {
+
+    var args = Array.prototype.slice.call(arguments, 0),
+      argv = module.exports.get.__argv || process.argv,
+      env = module.exports.get.__env || process.env,
+      parsedOptions = {};
+
+    argv.forEach(function(arg) {
+
+      if (arg.slice(0, 2) === '--') {
+
+        arg = arg.replace(/^\-{1,2}/, '');
+
+        var kv = arg.split(/=/),
+          key = camelize(kv[0]),
+          val = kv.length > 1 ? kv[1] : true;
+
+        if (schema.hasOwnProperty(key)) {
+          parsedOptions[key] = dejsonize(val);
+        }
+
+      }
+
+    });
+
+    Object.keys(schema).forEach(function(option) {
+
+      var envizedOption = envize(option);
+      if ( !parsedOptions.hasOwnProperty(option) ) {
+
+        if ( env.hasOwnProperty(envizedOption) ) {
+
+          // get from env.
+          parsedOptions[option] = dejsonize(env[envizedOption]);
+
+        } else if ( schema[option].hasOwnProperty('default') ) {
+
+          // get from defaults.
+          parsedOptions[option] = schema[option].default;
+
+        } else if ( schema[option].hasOwnProperty('required') ) {
+          throw new Error('Missing required option ' + option);
+        }
+
+      }
+
+    });
+
+    return parsedOptions;
+
+  },
+
+  usage: function(name, desc, schema) {
+
+    var usageStr = '\n' + desc + '\n' +
+      'Usage: ' + name + ' options \n\nOptions:\n\n';
+
+    Object.keys(schema).forEach(function(option) {
+
+      var optionStr = '--' + option;
+
+      if (schema[option].default) {
+        optionStr += '=' + JSON.stringify(schema[option].default);
+      }
+
+      if (!schema[option].required) {
+        optionStr = '[ ' + optionStr + ' ]';
+      }
+
+      if (schema[option].description) {
+        optionStr = optionStr + ' : ' + schema[option].description;
+      }
+
+
+
+      usageStr += '\t' + optionStr + '\n';
+
+    });
+
+    usageStr += '\n' + 'Any of the parameters can also be set by environment variables:\n\n';
+
+    Object.keys(schema).forEach(function(option) {
+
+      usageStr += '\t' + envize(option);
+
+      if (schema[option].default) {
+        usageStr += '=' + schema[option].default;
+      }
+
+      if (schema[option].description) {
+        usageStr += ' : ' + schema[option].description;
+      }
+
+      usageStr += '\n';
+
+    });
+
+    return usageStr;
+
+  }
+
+};
+
---
+++ test/spec/core.js
@@ -0,0 +1,74 @@
+
+'use strict';
+
+var optometrist = require('../../index');
+
+var schema = {
+  temperature: {
+    description: 'Set the temperature of the system.',
+    required: true
+  },
+  incrementRadius: {
+    description: 'Increment the radius of the blower.',
+    default: true
+  },
+  scissors: {
+    description: 'How many pairs do you need?',
+    default: 'sixty-one'
+  },
+  brainSaladSurgery: {
+    description: 'Reverse the polarity of the neutron flow.'
+  },
+  pruneWithGloves: {
+    description: 'May prevent a rash if working on clematis plants.',
+    default: true
+  }
+};
+
+describe('Optometrist', function() {
+
+  describe('#get', function() {
+
+    before(function() {
+      optometrist.get.__argv = [ 'test', '--brain-salad-surgery=woffle', '--increment-radius=false' ];
+      optometrist.get.__env = { 'TEMPERATURE': 8, 'SCISSORS': 'eleventy' };
+    });
+
+    it('retrieves the settings from the right places into an object', function() {
+      var settings = optometrist.get(schema);
+
+      // test default settings
+      expect(settings.pruneWithGloves).to.equal(true);
+
+      // test grabbing settings from env
+      expect(settings.temperature).to.equal(8);
+
+      // test overriding settings from env
+      expect(settings.scissors).to.equal('eleventy');
+
+      // test override of default with argv
+      expect(settings.incrementRadius).to.equal(false);
+
+      // test grabbing settings from argv
+      expect(settings.brainSaladSurgery).to.equal('woffle');
+    });
+
+  });
+
+  describe('#usage', function() {
+
+    it('returns a string containing usage instructions for the CLI', function() {
+      var usage = optometrist.usage('test', 'A test description.', schema);
+
+      expect(usage).to.be.a('string');
+      expect(usage).to.contain(schema.temperature.description);
+      expect(usage).to.contain(schema.incrementRadius.description);
+      expect(usage).to.contain(schema.scissors.description);
+      expect(usage).to.contain(schema.brainSaladSurgery.description);
+      expect(usage).to.contain(schema.pruneWithGloves.description);
+
+    });
+
+  });
+
+});
